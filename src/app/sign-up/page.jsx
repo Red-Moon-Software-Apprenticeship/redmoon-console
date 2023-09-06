7c414c08de63965112020d4241a89ffe3d81112f
Max <mfong415@gmail.com>
2023-09-06 15:51:03 -0700

    Added basic routing and under construction page

---
+++ src/app/sign-up/page.jsx
@@ -0,0 +1,13 @@
+
+import Layout from '@/components/Layout/Layout'
+import UnderConstruction from '@/components/UnderConstruction'
+
+const SignUp =() => {
+  return (
+   <Layout>
+      <UnderConstruction/>
+   </Layout>
+  )
+}
+
+export default SignUp
