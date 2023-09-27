a3fb37d3d21fea56c41e7b843d6a71d9fb5b64b6
Max <mfong415@gmail.com>
2023-09-27 16:39:55 -0700

    Added prisma singleton

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+//Implements the Prisma client as a singleton, so that when the project hot reloads it doesn't create multiple instances of the server
+
+const globalForPrisma = {
+    prisma: PrismaClient | undefined
+}
+
+export const prisma = 
+    globalForPrisma ??
+    new PrismaClient({
+        log:['query']
+    })
+
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma
