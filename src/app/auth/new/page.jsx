2ac99ae0eb06f6cea9e58f1a90babb561bfe3412
Max <mfong415@gmail.com>
2023-10-01 09:50:29 -0700

    Implemented logic to elevate role to session token

--- src/app/auth/new/page.jsx
+++ src/app/auth/new/page.jsx
@@ -8,7 +8,7 @@ const NewUser = () => {
     <Layout>
         <div id='' className=''>
             Please test new user sign up in postman. Another applicant has been assigned the responsibilities of putting together the sign up form
-            <Link href={'https://github.com/Red-Moon-Software-Apprenticeship/redmoon-console/issues/3'} target='_blank'> See here</Link>
+            <Link href='https://github.com/Red-Moon-Software-Apprenticeship/redmoon-console/issues/3' target='_blank'> See here</Link>
         </div>
     </Layout>
   );
