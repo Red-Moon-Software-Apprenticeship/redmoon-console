7ad965714551c13bd958f57a56109852c8093c7d
Max <mfong415@gmail.com>
2023-10-03 11:10:37 -0700

    Code formatting

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -5,35 +5,35 @@ import RoleLockedNav from './RoleLockedNav';
 import AuthProvider from '../AuthProvider';
 
 const NavBar = () => {
-  
-  
+
+
   return (
     <nav>
-        <ul>
-          <li>
-            <Link href='/'>
-               Home 
-            </Link>
-          </li>
-          <li>
-            <Link href='/'>
-               About Red Moon
-            </Link>
+      <ul>
+        <li>
+          <Link href='/'>
+            Home
+          </Link>
         </li>
-          <li>
-            <Link href='/'>
-               Our Apprentices
-           </Link>
-          </li>
-          <li>
-            <Link href='/'>
-               Home 
-            </Link>
-          </li>
-          <AuthProvider>
-            <RoleLockedNav/>
-          </AuthProvider>
-        </ul> 
+        <li>
+          <Link href='/'>
+            About Red Moon
+          </Link>
+        </li>
+        <li>
+          <Link href='/'>
+            Our Apprentices
+          </Link>
+        </li>
+        <li>
+          <Link href='/'>
+            Home
+          </Link>
+        </li>
+        <AuthProvider>
+          <RoleLockedNav />
+        </AuthProvider>
+      </ul>
     </nav>
   )
 };
