885a8f65b7ab64cc26e2a553f854178d624fa6ff
Max <mfong415@gmail.com>
2023-10-06 11:42:06 -0700

    Added error handling for patch route

--- src/app/api/apprentice/patch.js
+++ src/app/api/apprentice/patch.js
@@ -1,7 +1,22 @@
 import { updateAppr } from "@/database/users/updateAppr";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 const handler = async(req) => {
+
+    const session = await getServerSession(authOptions);
+    const sessionUser = session?.user
+    if ( !session || 
+         !sessionUser || 
+         (sessionUser.role !== 'admin' && !sessionUser.id !== req.userId ) ||
+         sessionUser.role !== 'admin'
+         ){
+
+        return NextResponse.json({msg: "Unauthorized access."}, {status: 401})
+
+    }
+
 
     try{
         const data = await req.json()
