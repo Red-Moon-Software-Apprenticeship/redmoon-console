97c3756b7ef693b61bc9ce6c6720b6b44c1e540f
Max <mfong415@gmail.com>
2023-10-11 10:49:56 -0700

    Code cleanup

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,6 +1,4 @@
 "use client"
-
-import { createReq } from '@/lib/createReqObj';
 import React, { useState } from 'react';
 import ApprSignUp from './ApprSignUp';
 import CompanySignUp from './CompanySignUp';
@@ -8,23 +6,10 @@ import CompanySignUp from './CompanySignUp';
 //Source is being used to track if this comes from the admin page or not, if it is, it defaults to apprentice options
 
 //Create specific child components that display options based on who is coming through: if its a company, show the items relevent to a company, appr, show appr.  
-const SignupForm = ({src = '', defaultRole ='apprentice'}) => {
+const SignupForm = ({}) => {
     
     const [role, setRole] = useState(defaultRole);
 
-    
-
-    //Specifc to apprentice applications
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-
-
-    //Specific to companies
-    const [address, setAddress] = useState('');
-
-    //Belongs to both
-    const [techStack, setTechStack] = useState([]);
-    const [bio, setBio] = useState('');
 
     const handleSwapForm = e =>{
         e.stopPropagation()
