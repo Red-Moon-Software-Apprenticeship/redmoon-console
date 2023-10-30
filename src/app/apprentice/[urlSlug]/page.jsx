f4323824d18fd11f6ca5bc8cd3860db29daed719
Max <mfong415@gmail.com>
2023-10-30 08:12:56 -0700

    Added basic styling

--- src/app/apprentice/[urlSlug]/page.jsx
+++ src/app/apprentice/[urlSlug]/page.jsx
@@ -3,23 +3,26 @@ import Layout from '@/components/Layout/Layout';
 import './profile.css'
 import { findApprBySlug } from '@/database/users/findUser';
 
-const Profile = async ({params}) => { 
-    
+const Profile = async ({ params }) => {
     const appr = await findApprBySlug(params.urlSlug);
-    const {image, name, email, city, state, techStack, bio} = appr;
+    const { image, name, email, city, state, techStack, bio } = appr;
+
     return (
         <Layout>
             <div className="profile-container">
-                <img src={appr.image} alt={appr.name} className="profile-image" />
-                <h1>{`${name}`}</h1>
-                <p>Email: {email}</p>
-                <p>Location: {`${city}, ${state}`}</p>
-                <p>Tech Stack: {techStack.join(', ')}</p>
-                <p>Bio: {bio || 'No bio provided'}</p>
+                <div className="profile-left">
+                    <img src={image} alt={name} className="profile-image" />
+                </div>
+                <div className="profile-right">
+                    <h1>{name}</h1>
+                    <p>Email: {email}</p>
+                    <p>Location: {`${city}, ${state}`}</p>
+                    <p>Tech Stack: {techStack.join(', ')}</p>
+                    <p>Bio: {bio || 'No bio provided'}</p>
+                </div>
             </div>
         </Layout>
     );
 };
-
 
 export default Profile;
