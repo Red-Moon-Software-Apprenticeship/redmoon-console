342f680499f06710ae5cd316102f3196a4d4c9ac
Max <mfong415@gmail.com>
2023-11-10 14:00:06 -0800

    Temp

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '@/components/Layout/Layout';
+import UnderConstruction from '@/components/UnderConstruction';
+
+const About = () => {
+
+  return (
+    <Layout> 
+        <UnderConstruction/>
+    </Layout>
+    );
+};
+
+export default About;
