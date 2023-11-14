d03a1de5ac091b14c4a0bb768ec40a86fca498b9
Max <mfong415@gmail.com>
2023-11-14 11:26:29 -0800

    Redeploy for test

--- src/hooks/useModalFactory.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ToggleModalBtn from '@/components/ToggleModalBtn';
-export const useModalFactory = (innerText, ModalComponent, ...modalProps) => {
-  
-  return [<ToggleModalBtn innerText={innerText} modal={<ModalComponent />} modalProps={modalProps}/>]
-};
-
-export default useModalFactory;
